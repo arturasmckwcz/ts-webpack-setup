9cfb9451d7e6f8026fa8e56474c72ae64ef5d746
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-29 20:46:02 +0100

    removed container for config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
-export const container = document.querySelector(".app");
 export const colors = [100, 200, 400, 700, 900];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import "./style.css";
-import { container, colors } from "./config";
-import { createBox, getBackgroundColor } from "./utils";
+import './style.css';
+import { container, colors } from './config';
+import { createBox, getBackgroundColor } from './utils';
 
-console.log("Hello World!");
-const boxes = colors.map((clr) => createBox(clr, container));
-boxes.forEach((box) => {
+console.log('Hello World!');
+const container = document.querySelector(".app");
+const boxes = colors.map(clr => createBox(clr, container));
+boxes.forEach(box => {
   box.textContent = getBackgroundColor(box);
 });
